12512512b2ce67fd30ce2cfe2b76ef824e5ebece
Thitiwat <thitiwat6338@gmail.com>
2018-05-08 16:51:13 +0700

    array Operato

---
+++ src/lab5/arrayOperator.java
@@ -0,0 +1,8 @@
+package lab5;
+//1.method for input data
+//2.method for show data in array
+//3.method to find average value in array
+//4.method to find maximum value in array
+//5.method to find maximum value in array
+public class arrayOperator {
+}
