928aeb1479cdc4e27d9ad452f7170267f7357d6e
Thitiwat <thitiwat6338@gmail.com>
2018-05-08 15:13:38 +0700

    Update data

--- src/OOPMIT/Hello.java
+++ src/OOPMIT/Hello.java
@@ -4,5 +4,8 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello, My name is Thitiwat");
+        System.out.println("Major: Information Systest");
+        System.out.println("Faculty: Management Technology");
+        System.out.println("University: RMUTSV");
     }
 }
